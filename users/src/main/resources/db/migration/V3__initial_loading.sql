ee4a03aaf3285dbe68928f8e6f7589010cd2dbf5
Adriano Brito <adrianosdebrito@gmail.com>
2017-12-15 21:06:07 -0300

    [Database] Add migrations to initialize data in users module

---
+++ users/src/main/resources/db/migration/V3__initial_loading.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS VALUES(default, 'adrianos', 'password', 'Adriano');
+INSERT INTO USERS VALUES(default, 'mozinho', 'password', 'Mozinho');
+INSERT INTO USERS VALUES(default, 'twinpeaks', 'password', 'Twin Peaks');
+INSERT INTO USERS VALUES(default, 'concluded', 'password', 'Concluded');
+INSERT INTO USERS VALUES(default, 'iaemen', 'password', 'Iae Men');
