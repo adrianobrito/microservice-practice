19a6e11a6feddccc5c943cfb766fd9e6c4bacc07
Adriano Brito <adrianosdebrito@gmail.com>
2017-12-18 10:44:55 -0300

    [Setup] Setup places as configuration server client

--- places/src/main/java/com/adrianobrito/places/controller/PlaceController.java
+++ places/src/main/java/com/adrianobrito/places/controller/PlaceController.java
@@ -6,11 +6,22 @@ import java.util.Random;
 import com.adrianobrito.places.domain.Place;
 import com.adrianobrito.places.repository.PlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("places")
+@RefreshScope
+@RestController
+@RequestMapping("places")
 public class PlaceController {
+
+    @Value("${places.message}")
+    private String placesMessage;
+
+    @Value("${message}")
+    private String message;
 
     private PlaceRepository placeRepository;
     private Random random;
@@ -30,6 +41,16 @@ public class PlaceController {
     public Place getRandomPlace() {
         List<Place> places = placeRepository.listPlaces();
         return places.get(random.nextInt(places.size()));
+    }
+
+    @GetMapping("message")
+    public String getMessage() {
+        return message;
+    }
+
+    @GetMapping("placesMessage")
+    public String getPlacesMessage() {
+        return placesMessage;
     }
 
 }
