534f8f9a601ebc4d34bf16019767a37cfbe7c98f
Adriano Brito <adrianosdebrito@gmail.com>
2017-12-18 17:32:57 -0300

    [Setup] Set necessary configuration to put Eureka up and running

--- places/src/main/java/com/adrianobrito/places/PlacesApplication.java
+++ places/src/main/java/com/adrianobrito/places/PlacesApplication.java
@@ -2,8 +2,10 @@ package com.adrianobrito.places;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlacesApplication {
 
 	public static void main(String[] args) {
