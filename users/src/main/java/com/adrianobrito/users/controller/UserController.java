a4e06f6fb86927c98b735cc5bcb6a5c079aecc3d
Adriano Brito <adrianosdebrito@gmail.com>
2017-12-18 11:14:43 -0300

    [Setup] Migrate all projects to remote configuration

--- users/src/main/java/com/adrianobrito/users/controller/UserController.java
+++ users/src/main/java/com/adrianobrito/users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.adrianobrito.users.controller;
 import com.adrianobrito.users.domain.User;
 import com.adrianobrito.users.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
